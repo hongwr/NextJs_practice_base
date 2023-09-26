cab386fafc90d526b1a08fe97e18a21717c24eb9
HongWangyeol <109332847+HongWangyeol@users.noreply.github.com>
2023-09-26 17:14:20 +0900

    add error

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,25 +1,25 @@
 import getProducts from '@/service/products';
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 import MeowArticle from '@/components/MeowArticle';
 
 // ISR Rendering, 3초마다 re rendering
 // export const revalidate = 3;
 
 export default async function ProductsPage() {
-  const products = await getProducts();
+    const products = await getProducts();
 
-  return (
-    <>
-        <h1>제품 소개 페이지!</h1>
-        <ul>
-            {products.map((product, index) => (
+    return (
+        <>
+            <h1>제품 소개 페이지!</h1>
+            <ul>
+                {products.map((product, index) => (
                     <li key={index}>
                         <Link href={`/product/${product.id}`}>{product.name}</Link>
                     </li>
-            ))}
-        </ul>
-              <MeowArticle />
-    </>
-  )
+                ))}
+            </ul>
+            <MeowArticle />
+        </>
+    );
 }
